444886cb6f0c79fbde189d79ac78d2e3a485ffa7
cloudstitch-visualizations <cloudstitch.visualizations@gmail.com>
2015-02-06 13:45:49 -0500

    Update map.js

--- map.js
+++ map.js
@@ -68,7 +68,7 @@ CTS.status.libraryLoaded.then(function() {
       shadedUsaMapWidget_Init(widgetContainer);
     };
     var tryIt = function() {
-      if (typeof d3 != 'undefined') {
+      if ((typeof d3 != 'undefined') && (typeof window.shadedUsaMapWidget_Data != 'undefined')) {
         doit();
       } else {
         setTimeout(tryIt, 100);
